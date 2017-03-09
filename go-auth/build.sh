4508d777bbbdf3fb0161038ac97f8f507d0a5f9d
Uğur Özşahin <ugurozsahin@live.com>
2017-03-10 01:41:56 +0300

    docker build cannot enable tty mode on non tty input fix

--- go-auth/build.sh
+++ go-auth/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh 
 set -e 
-docker run --rm -it \
+docker run --rm -i \
     -v $PWD:/go/src/go-messenger/go-auth/ \
     -e SOURCE_PATH=go-messenger/go-auth/ \
     usman/go-builder:1.8
 
-docker build -t usman/go-auth .
+docker build -i usman/go-auth .
