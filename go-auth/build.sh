df9aa57121652f4880bf9ac1f0275e809ab92638
Uğur Özşahin <ugurozsahin@live.com>
2017-03-11 20:49:55 +0300

    build.sh fix!

--- go-auth/build.sh
+++ go-auth/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 docker run --rm -it \
-    -v $PWD:/go/src/github.com/ugurozsahin/go-messenger/go-auth/ \
-    -e SOURCE_PATH=github.com/ugurozsahin/go-messenger/go-auth/ \
+    -v $PWD:/go/src/go-messenger/go-auth/ \
+    -e SOURCE_PATH=go-messenger/go-auth/ \
     usman/go-builder:1.4 bash
 
 docker build -t ugurozsahin/go-auth .
